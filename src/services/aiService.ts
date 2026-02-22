8e4d0295e861c7a75ea943a1c5234c1ab7d785ec
GaneshRam05 <ganeshramm.bit22@rathinam.in>
2026-02-22 23:04:44 +0530

    first commit

--- src/services/aiService.ts
+++ src/services/aiService.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://blind-ai-backend.onrender.com/ask-ai";
 
 export const askAI = async (message: string): Promise<string> => {
   try {
